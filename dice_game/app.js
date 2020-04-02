8c4643166f712cdb206682be14a323964b82dd02
Karthik kumar <mkarthikkumar19@gmail.com>
2020-04-02 20:03:13 +0530

    task completed another dice added

--- dice_game/app.js
+++ dice_game/app.js
@@ -15,21 +15,28 @@ function nxtplayer (){
    document.querySelector('.player-1-panel').classList.toggle('active');
 
    document.querySelector('.dice').style.display = 'none';
+   document.querySelector('.dice2').style.display ='none';
 }
 
 function btn(){
    if(gamePlaying){
-      var dice = Math.floor(Math.random() * 6) + 1;
-      var diceDOM = document.querySelector('.dice');
-   diceDOM.style.display = 'block';
-   diceDOM.src = 'dice-' + dice + '.png';
-   if(dice != 1){
-      roundScore += dice;
+      var dice1 = Math.floor(Math.random() * 6) + 1;
+      var dice2 = Math.floor(Math.random() * 6) + 1;
+      document.querySelector('.dice').style.display = 'block';
+      document.querySelector('.dice').src = 'dice-' + dice1 + '.png';
+      document.querySelector('.dice2').style.display = 'block';
+      document.querySelector('.dice2').src = 'dice-' + dice2 + '.png';
+
+  
+
+    if(dice1 != 1 && dice2 != 1){
+      roundScore += dice1 + dice2;
       document.querySelector('#current-' + activePlayer).textContent = roundScore;
    }
    else{
      nxtplayer();
    }
+   lastdice = dice;
    }
 }
 
@@ -38,10 +45,20 @@ function hold(){
       scores[activePlayer] += roundScore;
 
       document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+      var input = document.querySelector('.final-score').value;
+      var winningScore;
+
+      if(input){
+         winningScore = input;
+      }else{
+         winningScore = 100;
+      }
    
-      if(scores[activePlayer] >= 20){
+      if(scores[activePlayer] >= winningScore){
          document.querySelector('#name-' + activePlayer).textContent = 'Winner';
          document.querySelector('.dice').style.display = 'none';
+         document.querySelector('.dice2').style.display = 'none';
          document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
          document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
          gamePlaying = false;
@@ -54,9 +71,11 @@ function hold(){
 function init(){
    scores=[0,0];
    roundScore=0;
-   activePlayer=0;
+   activePlayer=0;dummy=0;
    gamePlaying = true;
 
+   document.querySelector('.dice').style.display = 'none';
+   document.querySelector('.dice2').style.display ='none';
 document.getElementById('score-0').textContent = 0;
 document.getElementById('score-1').textContent = 0;
 document.getElementById('current-0').textContent = 0;
@@ -66,7 +85,7 @@ document.getElementById('name-1').textContent = 'Player 2';
 document.querySelector('.player-0-panel').classList.remove('winner');
 document.querySelector('.player-1-panel').classList.remove('winner');
 document.querySelector('.player-0-panel').classList.remove('active');
-document.querySelector('.player-0-panel').classList.remove('active');
+document.querySelector('.player-1-panel').classList.remove('active');
 document.querySelector('.player-0-panel').classList.add('active');
 
 }
