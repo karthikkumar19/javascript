f0a635cfd7de07397ab85804b12bd8a007a5601a
Karthik kumar <mkarthikkumar19@gmail.com>
2020-04-15 17:20:46 +0530

    babel new versions updated

--- forkify/src/js/index.js
+++ forkify/src/js/index.js
@@ -1,6 +1,5 @@
 // Global app controller
-import num from './test'; 
 
 const x = 32;
 
-console.log(`i imported ${num} from test js variable is ${x}`);
+console.log(`i imported from test js variable is ss$`);
--- forkify/src/js/test.js
+++
@@ -1,2 +0,0 @@
-console.log('inported module');
-export default 23;
